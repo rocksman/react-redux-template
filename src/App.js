d1ffd48928e95bfcb53f4fe9335ce4bd6fea2cef
Ron Mathew Sam <ronsam98@gmail.com>
2020-08-13 15:42:54 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
+import { Provider } from "react-redux";
+import store from "./stores";
+import { BrowserRouter, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route exact strict path="/" component={()=><h1>Home</h1>} />
+        <Route exact strict path="/:id" component={()=><h1>Room</h1>} />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
